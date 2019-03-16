f281030a18455e9d9dd465e01769fcff192ee985
Scott Haney <emailalmondbranch@gmail.com>
2019-03-15 22:31:42 -0400

    Extract out the stopping criteria into a separate class

--- src/Eigen.jl
+++ src/Eigen.jl
@@ -1,10 +1,23 @@
 module Eigen
 
-function powermethod(Matrix::AbstractMatrix, Guess::AbstractMatrix)
+abstract type IterativeStoppingCritera
+end
+
+mutable struct ExactlyNIterations <: IterativeStoppingCritera
+    n::Integer
+end
+
+function ShouldStop(N::ExactlyNIterations, IterationsExecuted::Integer)
+    return IterationsExecuted >= N.n
+end
+
+function powermethod(Matrix::AbstractMatrix, Guess::AbstractMatrix, StoppingCriteria::IterativeStoppingCritera)
     current = Guess
-    iterations = 25
-    for n in 1:iterations
+    iteration = 0
+
+    while !ShouldStop(StoppingCriteria, iteration)
         current = Matrix * current
+        iteration = iteration + 1
     end
 
     return current
