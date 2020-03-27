498533fd6468cdb176466cede6799afa6e05f357
Scott Haney <emailalmondbranch@gmail.com>
2020-03-26 21:20:26 -0400

    Add another test for arnoldi

--- test/arnoldi.jl
+++ test/arnoldi.jl
@@ -1,10 +1,21 @@
-@testset "Finds a trivial arnoldi decomposition" begin
+@testset "Finds a trivial complete arnoldi decomposition" begin
 
     matrix = [1 1 1;1 1 1;0 1 1];
     guess = [1;0;0]
     
     expected = [1 1 1;1 1 1;0 1 1]
     actual = Eigen.arnoldi(matrix, guess, 3)
+
+    @test expected == actual
+end
+
+@testset "Finds a trivial partial arnoldi decomposition" begin
+
+    matrix = [1 1 1;1 1 1;0 1 1];
+    guess = [1;0;0]
+    
+    expected = [1 1;1 1;0 1]
+    actual = Eigen.arnoldi(matrix, guess, 2)
 
     @test expected == actual
 end
