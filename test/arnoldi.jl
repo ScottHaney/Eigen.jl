2808ddad74a7dad4071d5d157531d3bc30223b86
Scott Haney <emailalmondbranch@gmail.com>
2020-04-16 19:08:04 -0400

    Add a test for the arnoldi method if it stops short

--- test/arnoldi.jl
+++ test/arnoldi.jl
@@ -19,3 +19,14 @@ end
 
     @test expected == actual.H
 end
+
+@testset "Stops the decomposition if an invariant subspace is found" begin
+
+    matrix = [1 0 0;0 1 0;0 0 1]
+    guess = [1;0;0]
+    
+    expected = 1
+    actual = Eigen.arnoldi(matrix, guess, 2)
+
+    @test expected == actual.numColumns
+end
